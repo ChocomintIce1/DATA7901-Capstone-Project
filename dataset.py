1f007af3e76b449d2e467aa507ebb3dd7c373cb7
ChocomintIce1 <69633077+ChocomintIce1@users.noreply.github.com>
2024-03-10 01:43:59 +1000

    Created a function that splits the dataset for training

--- dataset.py
+++ dataset.py
@@ -1,7 +1,8 @@
 import pandas as pd
+from sklearn.model_selection import train_test_split
 
 
-def get_dataset() -> tuple:
+def get_dataset() -> pd.DataFrame:
     """
     Reads the csv file and returns the dataframe.
 
@@ -13,3 +14,22 @@ def get_dataset() -> tuple:
     y = df['winner']
 
     return X, y
+
+def test_train(train_split=0.7, validation_split=0.2) -> pd.DataFrame:
+    """
+    Creates training, validation and testing datasets.
+    Initially configured to a 70-20-10 split
+
+    Returns:
+        tuple: Returns a tuple of (X_train, X_validation, ..., y_test) 
+    """
+    X, y = get_dataset()
+    validation_split = validation_split/train_split
+    print('n:', X.shape[0])
+
+    X_train, X_validation, y_train, y_validation = train_test_split(X, y, test_size=1-train_split)
+    X_validation, X_test, y_validation, y_test = train_test_split(X_validation, y_validation, test_size=validation_split)
+
+    print(X_train.shape[0], X_validation.shape[0], X_test.shape[0])
+
+    return X_train, X_validation, X_test, y_train, y_validation, y_test
