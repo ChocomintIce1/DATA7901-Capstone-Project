1396108f91ce7b5430492a392991bf85c4787d4c
ChocomintIce1 <69633077+ChocomintIce1@users.noreply.github.com>
2024-03-10 21:55:48 +1000

    Added correlation matrix method

--- dataset.py
+++ dataset.py
@@ -1,4 +1,7 @@
 import pandas as pd
+import matplotlib.pyplot as plt
+import seaborn as sns
+import numpy as np
 from sklearn.model_selection import train_test_split
 from sklearn.preprocessing import normalize
 
@@ -35,3 +38,31 @@ def test_train(train_split=0.7, validation_split=0.2, normalise=True) -> pd.Data
     X_validation, X_test, y_validation, y_test = train_test_split(X_validation, y_validation, test_size=validation_split)
 
     return X_train, X_validation, X_test, y_train, y_validation, y_test
+
+def plot_correlation_matrix() -> None:
+    """
+    Reference: https://seaborn.pydata.org/examples/many_pairwise_correlations.html
+    Plots the correlation matrix between the predictor variables
+
+    Returns:
+        None
+    """
+    X, _ = get_dataset()
+
+    # Set theme
+    sns.set_theme(style="white")
+
+    # Compute correlation matrix
+    corr = X.corr()
+
+    # Generate a mask for the upper triangle
+    mask = np.triu(np.ones_like(corr, dtype=bool))
+
+    # Generate a custom diverging colormap
+    cmap = sns.diverging_palette(230, 20, as_cmap=True)
+
+    sns.heatmap(corr, mask=mask, cmap=cmap, vmax=.3, center=0,
+            square=True, linewidths=.5, cbar_kws={"shrink": .5})
+    
+    # Plot graph
+    plt.show()
