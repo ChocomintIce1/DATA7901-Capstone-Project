2103de2f47e4b70876385cc46cbe0976d3755d29
ChocomintIce1 <69633077+ChocomintIce1@users.noreply.github.com>
2024-04-05 15:03:14 +1000

    moved code to correct file

--- predict.py
+++
@@ -1,8 +0,0 @@
-from dataset import *
-from modules import RNN
-
-
-# Load data
-df = read_processed_dataset()
-
-# league_dataset = LeagueDataset()
---
+++ train.py
@@ -0,0 +1,8 @@
+from dataset import *
+from modules import RNN
+
+
+# Load data
+df = read_processed_dataset()
+
+# league_dataset = LeagueDataset()
