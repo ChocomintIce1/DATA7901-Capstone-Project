af3b590a3d1817f7aa97ecc3c80219ed95ccf408
ChocomintIce1 <69633077+ChocomintIce1@users.noreply.github.com>
2024-04-27 02:30:12 +1000

    created basic predicct.py code

--- train.py
+++ train.py
@@ -44,44 +44,16 @@ optimiser = torch.optim.Adam(rnn.parameters(), lr=lr)
 loss_function = torch.nn.CrossEntropyLoss()
 
 # Training model
-epochs = 10
-
-for epoch in range(epochs):
-    print(f"--------- Epoch #{epoch} ---------")
-    for batch, (train, outcome) in enumerate(train_set):
-        train = torch.autograd.Variable(train).to(device)
-        outcome = torch.autograd.Variable(outcome).to(device, dtype=torch.long)
-
-        predict = rnn(train)
-        # print('printing shape', predict.size(0), outcome.size(0))
-        # print('predict', predict)
-
-        # Reformat outcome tensor
-        outcome1 = []
-        for o in outcome:
-            outcome1.append(o)
-        outcome = torch.LongTensor(outcome1)
-
-        loss = loss_function(predict, outcome)
-
-        optimiser.zero_grad()
-        loss.backward()
-        optimiser.step()
-
-        if batch % 30 == 0:
-            loss, current = loss.item(), batch*len(X)
-            print(f'loss: {loss} [{current}/{len(train_set.dataset)}]')
-    
-    # Testing
-    rnn.eval()
-    size = len(validation_set.dataset)
-
-    correct = 0
-    test_loss = 0
-    with torch.no_grad():
-        for batch, (validation, outcome) in enumerate(validation_set):
-            validation = torch.autograd.Variable(validation).to(device)
+def train(epochs=10):
+    for epoch in range(epochs):
+        print(f"--------- Epoch #{epoch} ---------")
+        for batch, (train, outcome) in enumerate(train_set):
+            train = torch.autograd.Variable(train).to(device)
             outcome = torch.autograd.Variable(outcome).to(device, dtype=torch.long)
+
+            predict = rnn(train)
+            # print('printing shape', predict.size(0), outcome.size(0))
+            # print('predict', predict)
 
             # Reformat outcome tensor
             outcome1 = []
@@ -89,15 +61,44 @@ for epoch in range(epochs):
                 outcome1.append(o)
             outcome = torch.LongTensor(outcome1)
 
-            predict = rnn(validation)
-            test_loss += loss_function(predict, outcome).item()
-            correct += (predict.argmax(1) == outcome).sum().item()
+            loss = loss_function(predict, outcome)
 
-        print(f"Acc:{correct/size:>7f}, Avg Loss: {test_loss/size:>7f}")
+            optimiser.zero_grad()
+            loss.backward()
+            optimiser.step()
+
+            if batch % 30 == 0:
+                loss, current = loss.item(), batch*len(X)
+                print(f'loss: {loss} [{current}/{len(train_set.dataset)}]')
+        
+        # Testing
+        rnn.eval()
+        size = len(validation_set.dataset)
+
+        correct = 0
+        test_loss = 0
+        with torch.no_grad():
+            for batch, (validation, outcome) in enumerate(validation_set):
+                validation = torch.autograd.Variable(validation).to(device)
+                outcome = torch.autograd.Variable(outcome).to(device, dtype=torch.long)
+
+                # Reformat outcome tensor
+                outcome1 = []
+                for o in outcome:
+                    outcome1.append(o)
+                outcome = torch.LongTensor(outcome1)
+
+                predict = rnn(validation)
+                test_loss += loss_function(predict, outcome).item()
+                correct += (predict.argmax(1) == outcome).sum().item()
+
+            print(f"Acc:{correct/size:>7f}, Avg Loss: {test_loss/size:>7f}")
 
 # Save the model
-save_model = True
+if __name__ == '__main__':
+    train()
+    save_model = True
 
-if save_model:
-    print('Model saved')
-    torch.save(rnn, 'League_of_Legends_predicition.pt')
+    if save_model:
+        print('Model saved')
+        torch.save(rnn, 'League_of_Legends_predicition.pt')
