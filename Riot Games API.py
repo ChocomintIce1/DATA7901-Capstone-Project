e2ee37b9aba671de3d247472b1c05f3b3d6ab210
ChocomintIce1 <69633077+ChocomintIce1@users.noreply.github.com>
2024-10-10 02:02:09 +1000

    added a way of fetching online matches

--- Riot Games API.py
+++ Riot Games API.py
@@ -1,14 +1,84 @@
-import requests
+import ast
+import re
+from selenium import webdriver # Version 3.3.0
 
-API_KEY = open(r"C:\Users\Jae\Desktop\api_key.txt")
-puuid = "jbsmMdaWH94ZhqeO2A6U_i7cS0JYtDjHSZJ_bNluxYZfgg3_E0GaSNWU4r_GYELvIlAULOZmo6u_eA"
-match_history_url = "https://sea.api.riotgames.com/lol/match/v5/matches/by-puuid/MlPhG7YdJyzwZ6hLGDBFgGshR6TjJaiPX_kOehD8kfh7178PXJ14ZTjISLZFbwLOB5iEJ39g9JutZw/ids?start=0&count=20&api_key=RGAPI-623ee5ec-5af3-4642-8dcc-cf616b888a8e"
-match_timeline_url = "https://sea.api.riotgames.com/lol/match/v5/matches/OC1_639113578/timeline?api_key=RGAPI-623ee5ec-5af3-4642-8dcc-cf616b888a8e"
 
-response = requests.get(match_history_url).json()
-match_id = response[0]
-print(match_id)
+driver = webdriver.PhantomJS(r"C:\Users\Jae\Documents\phantomjs-2.1.1-windows\bin\phantomjs.exe")
 
-match_timeline_info = requests.get(match_timeline_url).json()
-match_timeline_info = match_timeline_info["info"]
-print(match_timeline_info.keys())
+# Timeline
+driver.get("https://gol.gg/game/stats/44421/page-timeline/")
+html_page_source = driver.page_source
+
+# Important variable names
+golddatas = "golddatas"
+Events = "Events"
+csdatas  = "csdatas"
+
+blue_side = "../_img/blueside-icon.png"
+red_side = "../_img/redside-icon.png"
+
+# Variables
+count = 0
+role = None
+data = []
+unprocessed_events = []
+
+
+def process_events(event_string):
+    pass
+
+# Process source
+# if count <= 4 -> blue, count >=5 -> red
+html_page_source_list = html_page_source.split("\n")
+inside_golddatas = False
+inside_events = False
+
+for html_source_string in html_page_source_list:
+    # Process gold data
+    # If string is gold information
+    if golddatas in html_source_string:
+        inside_golddatas = True
+    
+    if inside_golddatas:
+        if "label:" in html_source_string:
+            role =  html_source_string.split(" ")[3][1:-2]
+            # print("role:", role)
+            count += 1
+        
+        if "data:" in html_source_string:
+            data = html_source_string.split(" ")[3][:-2] + "]"
+            team = "blue" if count < 6 else "red"
+            data = ast.literal_eval(data)
+
+    # If all of gold information extracted
+    if csdatas in html_source_string:
+        inside_golddatas = False
+    
+    # Process events data
+    # If string is events information
+    if "Events" in html_source_string:
+        inside_events = True
+    
+    # Save events to a list
+    if inside_events:
+        if blue_side in html_source_string or red_side in html_source_string:
+            unprocessed_events += html_source_string.split("side-icon.png")
+
+    # If end of events
+    if "function ShowPoint" in html_source_string:
+        inside_events = False
+
+        # If list has been populated
+        print(unprocessed_events[0])
+        print("==========================")
+        print(unprocessed_events[1])
+        
+        # for event in unprocessed_events:
+        #     print(event)
+        #     print("==========================")
+
+print('Done')
+
+# s = "ap@ple#ban@ana#cherry#or@ange"
+
+# print(re.split('@|#', s))
